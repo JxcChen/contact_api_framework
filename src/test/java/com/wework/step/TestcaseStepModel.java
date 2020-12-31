dc8cf540dcdbb25bdc903a19955fb3996dca8421
chenjinxuan <360088940@qq.com>
2020-12-31 16:34:53 +0800

    完成TestcaseStepModel编写

--- src/test/java/com/wework/step/TestcaseStepModel.java
+++ src/test/java/com/wework/step/TestcaseStepModel.java
@@ -1,7 +1,18 @@
 package com.wework.step;
 
+
+
+import com.wework.api_object.ApiLoader;
+import com.wework.global.GlobalVariables;
+import com.wework.utils.PlaceholderUtils;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.function.Executable;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * ClassName: TestcaseStepModel
@@ -31,6 +42,100 @@ public class TestcaseStepModel {
     private HashMap<String,String> save;
     private HashMap<String,String> saveGlobal;
     // 需要创建assertModel
-    private ArrayList<AssertModel> assertModelList;
+    private ArrayList<AssertModel> asserts;
 
+    // 需要创建一个 stepResult 承接执行后返回的结果 里面需要封装响应 断言列表 用例变量
+    private StepResultModel stepResultModel = new StepResultModel();
+    private HashMap<String,String> stepVariables = new HashMap<>();
+    private ArrayList<String> finalActualParameter = new ArrayList<>();
+    private ArrayList<Executable> assertList = new ArrayList<>();
+    public StepResultModel run(HashMap<String,String> testcaseVariable){
+        // 1 先将实参中存在的占位符进行替换  时间戳
+        if (actualParameter != null){
+            finalActualParameter.addAll(PlaceholderUtils.resolveList(actualParameter,testcaseVariable));
+        }
+
+        // 2 通过ApiLoader 获取所有用例文件 在根据 api 和 action 拿到对应action执行run方法获取响应
+        Response response = ApiLoader.run(api, action).run(finalActualParameter);
+
+        // 3 存储变量
+        // 先存储局部变量
+        if (save != null){
+            save.forEach((variableKey,path) -> {
+                stepVariables.put(variableKey,response.path(path).toString());
+            });
+        }
+        // 存储全局变量
+        if (saveGlobal !=null){
+            saveGlobal.forEach((variableKey,path) ->{
+                GlobalVariables.getGlobalVariables().put(variableKey,response.path(path).toString());
+            });
+        }
+
+
+        // 4 封装断言列表
+        if (asserts != null){
+            asserts.forEach(assertModel -> {
+                assertList.add(() -> {
+                    assertThat(assertModel.getReason(),response.path(assertModel.getActual()),equalTo(assertModel.getExpect()));
+                });
+            });
+        }
+        // 5 封装result
+        stepResultModel.setAssertList(assertList);
+        stepResultModel.setStepVariables(stepVariables);
+        stepResultModel.setResponse(response);
+        return stepResultModel;
+
+    }
+
+
+
+    public String getApi() {
+        return api;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public ArrayList<String> getActualParameter() {
+        return actualParameter;
+    }
+
+    public void setActualParameter(ArrayList<String> actualParameter) {
+        this.actualParameter = actualParameter;
+    }
+
+    public HashMap<String, String> getSave() {
+        return save;
+    }
+
+    public void setSave(HashMap<String, String> save) {
+        this.save = save;
+    }
+
+    public HashMap<String, String> getSaveGlobal() {
+        return saveGlobal;
+    }
+
+    public void setSaveGlobal(HashMap<String, String> saveGlobal) {
+        this.saveGlobal = saveGlobal;
+    }
+
+    public ArrayList<AssertModel> getAsserts() {
+        return asserts;
+    }
+
+    public void setAsserts(ArrayList<AssertModel> asserts) {
+        this.asserts = asserts;
+    }
 }
---
+++ src/test/java/com/wework/testcase/TestcaseModel.java
@@ -0,0 +1,10 @@
+package com.wework.testcase;
+
+/**
+ * ClassName: TestcaseModel
+ * date: 2020/12/31 12:41
+ * @author JJJJ
+ * Description:  定义ApiTestCaseModel类，用来用来存储testcase yaml反序列化出来的TestCase实体对象
+ */
+public class TestcaseModel {
+}
