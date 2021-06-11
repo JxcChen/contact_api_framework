b153d200a4c1341583dd37fc0b319844c2036f9a
chenjinxuan <360088940@qq.com>
2021-06-11 17:09:15 +0800

    测试

--- src/test/java/com/gemdale/test/GetFormalParamTest.java
+++ src/test/java/com/gemdale/test/GetFormalParamTest.java
@@ -1,6 +1,8 @@
 package com.gemdale.test;
 
 import com.gemdale.utils.CustomStrUtils;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +13,12 @@ import java.util.ArrayList;
  * date:2021/6/1 16:51
  * Description: TODO
  */
+@Epic("测试用的")
+@Feature("测试写的用例")
 public class GetFormalParamTest {
 
     @Test
+    @Feature("简单测试")
     void test(){
         System.out.println("简单测试");
         Assertions.assertEquals(1,1);
